5678ed8e2c830bf7eeae58650478c553ba2a9495
MTzica-TI <matheustrabctt@gmail.com>
2025-06-12 12:33:04 -0300

    FIX

--- main.go
+++ main.go
@@ -24,27 +24,82 @@ func main() {
 	r.LoadHTMLFiles(files...)
 
 	cfg := helper.ReadConfig()
-	b := bot.GetBot()
+	b := bot.GetBot() // GetBot já faz b.Open(), não precisa abrir de novo!
 
+	// Remova ou comente esta linha:
+	// err = b.Open()
+	// if err != nil {
+	// 	fmt.Println("Erro ao conectar o bot:", err)
+	// 	return
+	// }
+	defer b.Close()
+	fmt.Println("Bot do Discord está online!")
+
+	// Registra comandos quando o bot fica pronto
 	b.AddHandlerOnce(func(s *discordgo.Session, r *discordgo.Ready) {
 		for _, guild := range s.State.Guilds {
-			_, err := s.ApplicationCommandCreate(cfg.Application, guild.ID, &discordgo.ApplicationCommand{
+			cmd, err := s.ApplicationCommandCreate(cfg.Application, guild.ID, &discordgo.ApplicationCommand{
 				Name:        "ping",
 				Description: "Responde com Pong!",
 			})
 			if err != nil {
 				fmt.Printf("Erro ao criar comando em guild %s: %v\n", guild.ID, err)
 			} else {
-				fmt.Printf("Comando /ping registrado em guild %s\n", guild.ID)
+				fmt.Printf("Comando /ping registrado em guild %s: %+v\n", guild.ID, cmd)
 			}
 		}
 
-		s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			if i.ApplicationCommandData().Name == "ping" {
+		// Adiciona o comando /ticket
+		for _, guild := range s.State.Guilds {
+			_, err := s.ApplicationCommandCreate(cfg.Application, guild.ID, &discordgo.ApplicationCommand{
+				Name:        "ticket71", // <-- novo nome aqui
+				Description: "Abre um ticket privado para suporte.",
+			})
+			if err != nil {
+				fmt.Printf("Erro ao criar comando /ticket em guild %s: %v\n", guild.ID, err)
+			} else {
+				fmt.Printf("Comando /ticket registrado em guild %s\n", guild.ID)
+			}
+		}
+	})
+
+	// REGISTRE O HANDLER DE INTERAÇÃO AQUI, FORA DO READY!
+	b.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.ApplicationCommandData().Name {
+		case "ping":
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Pong!",
+				},
+			})
+		case "ticket71": // <-- novo nome aqui
+			user := i.Member.User
+			guildID := i.GuildID
+			channelName := fmt.Sprintf("ticket-%s", user.Username)
+
+			// Cria canal privado
+			channel, err := s.GuildChannelCreateComplex(guildID, discordgo.GuildChannelCreateData{
+				Name: channelName,
+				Type: discordgo.ChannelTypeGuildText,
+				PermissionOverwrites: []*discordgo.PermissionOverwrite{
+					{
+						ID:   guildID,
+						Type: discordgo.PermissionOverwriteTypeRole,
+						Deny: discordgo.PermissionViewChannel,
+					},
+					{
+						ID:    user.ID,
+						Type:  discordgo.PermissionOverwriteTypeMember,
+						Allow: discordgo.PermissionViewChannel,
+					},
+				},
+			})
+			if err != nil {
 				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 					Type: discordgo.InteractionResponseChannelMessageWithSource,
 					Data: &discordgo.InteractionResponseData{
-						Content: "Pong!",
+						Content: "Erro ao criar o ticket.",
 					},
 				})
 				return
